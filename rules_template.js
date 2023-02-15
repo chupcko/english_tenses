6d280b60ad761eb03f96ef70096af7b7c53a4699
Goran Lazic <github@chupcko.org>
2023-02-15 22:20:00 +0100

    Renamed attributes

--- rules_template.js
+++ rules_template.js
@@ -1,10 +1,12 @@
 console.log('tenses = {');
-for(let x1 of [ 'Statement', 'Question' ]) {
-  for(let x2 of [ 'Positive', 'Negative' ]) {
-    for(let x3 of [ 'Past', 'Present', 'Future' ]) {
-      for(let x4 of [ 'Simple', 'Continuous', 'Perfect', 'PerfectContinuous' ]) {
-        for(let x5 of [ 'I', 'He She It', 'We You They' ]) {
-          console.log(`  '${ x1 } ${ x2 } ${ x3 } ${ x4 } ${ x5 }': '???',`);
+for(let x1 of [ 'statement', 'question' ]) {
+  for(let x2 of [ 'positive', 'negative' ]) {
+    for(let x3 of [ 'past', 'present', 'future' ]) {
+      for(let x4 of [ 'nonPerfect', 'perfect' ]) {
+        for(let x5 of [ 'simple', 'continuous' ]) {
+          for(let x6 of [ 'I', 'he she it', 'we you they' ]) {
+            console.log(`  '${ x1 } ${ x2 } ${ x3 } ${ x4 } ${ x5 } ${ x6 }': '???',`);
+          }
         }
       }
     }
